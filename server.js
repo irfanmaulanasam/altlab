ec9e0fa104e40a1dd833b144a6affbb946bbf9a0
irfan maulanasam <alternatiflab@gmail.com>
2020-02-25 00:33:00 +0700

    adding client

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-const express = require('express')
+var express = require('express')
+var cors = require('cors')
+var bodyParser = require('body-parser')
+var app = express()
+var port = process.env.PORT || 5000
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+)
+
+var Users = require('./routes/Users')
+
+app.use('/users', Users)
+
+app.listen(port, function() {
+  console.log('Server is running on port: ' + port)
+})
